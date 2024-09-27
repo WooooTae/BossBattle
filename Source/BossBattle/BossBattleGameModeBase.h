c1ee9a26e7391e2039fcc84161930f90fed0a4ff
xcvxcv203 <xcvxcv203@gmail.com>
2024-09-28 00:20:39 +0900

    초기 프로젝트 추가중

---
+++ Source/BossBattle/BossBattleGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BossBattleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BOSSBATTLE_API ABossBattleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
